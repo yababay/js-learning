10da37f32cc45c0aad610ad2919ccfa6fd4e7a9b
Dmitriy Mazurov <mazurovdimon@gmail.com>
2022-11-01 11:12:23 +0300

    Создание проекта с нуля

--- index.test.js
+++ index.test.js
@@ -1,10 +1,9 @@
 const helloWorld = require('.')
 
-test('Формирование фразы "Hello, World"!', () => {
-  expect(helloWorld()).toBe('Hello, World!')
+test('Формирование фразы "Hello, world"!', () => {
+  expect(helloWorld()).toBe('Hello, world!')
 })
 
 test('Формирование фразы "Привет, мир!"', () => {
   expect(helloWorld("Привет", "мир")).toBe('Привет, мир!')
 })
-
