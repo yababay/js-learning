6f56b7226ea3994d6f9a7afc81e4727801ad7c28
Dmitriy Mazurov <mazurovdimon@gmail.com>
2022-11-02 23:06:19 +0300

    First homework. Data types

--- 001. Типы в JavaScript/index.js
+++ 001. Типы в JavaScript/index.js
@@ -1,7 +1,26 @@
+module.exports = {checkNumber}
+
 function checkNumber(n){
     return typeof n === 'number'
 }
-module.exports = {checkNumber}
 
 // TODO: Разобраться с мудулями
 // TODO: Дописать функции для всех типов данных (7 типов)
+
+function types(n){
+    return typeof n
+}
+
+console.log(types('string')) // string
+console.log(types(10)) //number
+console.log(types(types)) // function
+console.log(types({})) // object
+let x 
+console.log(types(x)) // undefined
+x = true
+console.log(types(x)) // boolean
+x = !!"" // значение x = false
+console.log(x)
+x = Symbol("id")
+console.log(types(x)) // symbol
+
