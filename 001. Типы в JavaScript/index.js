fb8ef5973281d7f2b68c0f1982d3e13ac4340a04
Dmitriy Mazurov <mazurovdimon@gmail.com>
2022-11-04 10:41:42 +0300

    Добавлены функции для тестирования типов объектов, функций.

--- 001. Типы в JavaScript/index.js
+++ 001. Типы в JavaScript/index.js
@@ -1,26 +1,21 @@
-module.exports = {checkNumber}
+function checkFunction(fn){
+    return typeof fn === 'function'
+}
+
+function checkArray(arr){
+    return Array.isArray(arr)
+}
+
+function checkObject(obj){
+    return typeof obj === 'object'
+}
 
 function checkNumber(n){
     return typeof n === 'number'
 }
 
-// TODO: Разобраться с мудулями
-// TODO: Дописать функции для всех типов данных (7 типов)
-
-function types(n){
+function getType(n){
     return typeof n
 }
 
-console.log(types('string')) // string
-console.log(types(10)) //number
-console.log(types(types)) // function
-console.log(types({})) // object
-let x 
-console.log(types(x)) // undefined
-x = true
-console.log(types(x)) // boolean
-x = !!"" // значение x = false
-console.log(x)
-x = Symbol("id")
-console.log(types(x)) // symbol
-
+module.exports = {checkNumber, getType, checkObject, checkArray, checkFunction}
